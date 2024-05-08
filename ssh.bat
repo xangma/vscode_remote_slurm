ae604c9346627786941d11aded35c90a4d0a7626
xangma <xangma@users.noreply.github.com>
2024-05-08 13:11:15 +0100

    windows fix

--- ssh.bat
+++ ssh.bat
@@ -1,1 +1,2 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '~\ssh_wrapper.ps1' %*" 2>&1
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Users\user1\Downloads\vscode_remote_slurm\ssh_wrapper.ps1' %*" 2>&1 
+REM Must be full path to ssh_wrapper.ps1. Do not use ~!
